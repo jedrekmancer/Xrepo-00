6aac1d6b8335bc510cf49d823267f6e83d0b6d4f
jedrekmancer <andrzej.kaganek@gmail.com>
2022-10-12 03:48:31 +0200

    Dodałem Nowy Projekt Dupka1.0

---
+++ MyLittleSolution/Dupka1.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dupka1._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę dupek");
+            int liczbaDupek = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= liczbaDupek; i = i + 1)
+            {
+                Console.WriteLine(i + ". " + "Dupka");
+            }
+            Console.ReadLine();
+            
+
+            
+        }
+    }
+}
