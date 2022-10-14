2e440bfd372352f5ee575e57fa49b399242c6fe6
jedrekmancer <andrzej.kaganek@gmail.com>
2022-10-14 07:14:07 +0200

    Dodałem NOWY projekt JedrekSilnia! i program testowy do sprawdzania kodu TEST PROG

---
+++ MyLittleSolution/JedrekSilnia!/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JedrekSilnia_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Silnia zrobiona ze zwykłejn pętli
+            Console.WriteLine("Program zwraca silnię liczby naturalnej");
+            Console.WriteLine("Podaj liczbę:");
+            int s = int.Parse(Console.ReadLine());
+            int i, j;
+            j = 1;
+            
+            for (i = s; i > 0; i = i - 1)
+                {
+                    j = j * i;
+                }
+            int wynik;
+            wynik = j;
+
+
+            Console.WriteLine(wynik);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ MyLittleSolution/TEST PROG/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST_PROG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj liczbę: ");
+            int s = int.Parse(Console.ReadLine());
+            int k = s;
+            int[] dane = new int[s] ;
+            for (int i = 1; i < s ; i++)
+            {
+                dane[i-1] = i ;
+
+
+
+                k = k * dane[i - 1];
+                Console.WriteLine(dane[i-1]);
+                Console.WriteLine(k);
+            }
+
+
+
+
+
+            
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
+            
